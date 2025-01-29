7a601ec465edc949452442f12586c7d7edf64e60
Titoxd0919 <angelzavaleta223@gmail.com>
2025-01-28 19:23:14 -0600

    Agregar archivos de proyecto.

---
+++ Button_19795391/Button.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Button_19795391
+{
+    public partial class Button : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Mensaje(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            lblMessage.ForeColor = System.Drawing.Color.Green;
+            lblMessage.Text = "The email address " + txtEmailAddress.Text + " Was successfully saved.";
+        }
+    }
+}
